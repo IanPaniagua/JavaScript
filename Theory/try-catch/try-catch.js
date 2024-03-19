047a40b0a85b92ae99023f7f22f9cda015a194fa
IanPaniagua <paniagua.ian.de@gmail.com>
2024-03-19 12:03:06 +0100

    organized

---
+++ Theory/try-catch/try-catch.js
@@ -0,0 +1,9 @@
+'use strict'
+
+try {
+var year = 2019;
+alert(yea);  //hier is the error
+}catch{
+    alert("a courrido un error en el codigo");
+
+}
---
+++ try-catch/try-catch.js
@@ -0,0 +1,10 @@
+'use strict'
+
+try {
+var year = 2019;
+alert(yea);  //hier is the error
+decodeURIComponent("http:victorroblesweb.es");
+}catch{
+    alert("a courrido un error en el codigo");
+
+}
